51d3e8082cf3eec93615fcd7ca7a7faef36b59d4
DaVinCi <52623427+Blue-Davinci@users.noreply.github.com>
2024-07-06 16:08:14 +0300

    Added prettier shutdown that checks for running bacground routines, as well as cron Jobs

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func (app *application) server() error {
+	// declare our http server
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", app.config.port),
 		Handler:      app.routes(),
@@ -15,6 +20,41 @@ func (app *application) server() error {
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
+	// make a channel to listen for shutdown signals
+	shutdownChan := make(chan error)
+	// start a background routine, this will listen to any shutdown signals
+	go func() {
+		// make a quit channel
+		quit := make(chan os.Signal, 1)
+		// listen for the SIGINT and SIGTERM signals
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		// read signal from the quit channel and will wait till there is an actual signal
+		s := <-quit
+		// printout the signal details
+		app.logger.PrintInfo("shutting down server", map[string]string{
+			"signal": s.String(),
+		})
+		// make a 20sec context
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownChan <- err
+		}
+		// Log a message to say that we're waiting for any background goroutines to
+		// complete their tasks.
+		app.logger.PrintInfo("completing background tasks", map[string]string{
+			"addr": srv.Addr,
+		})
+		// wait for any background tasks to complete
+		app.wg.Wait()
+		// stop the cron job scheduler
+		ctx = app.config.notifier.cronJob.Stop()
+		<-ctx.Done()
+		// Call Shutdown() on our server, passing in the context we just made.
+		shutdownChan <- srv.Shutdown(ctx)
+	}()
+	// start the server printing out our main settings
 	app.logger.PrintInfo("starting server", map[string]string{
 		"addr": srv.Addr,
 		"env":  app.config.env,
@@ -24,5 +64,16 @@ func (app *application) server() error {
 			return err
 		}
 	}
+	// Otherwise, we wait to receive the return value from Shutdown() on the
+	// shutdownError channel. If return value is an error, we know that there was a
+	// problem with the graceful shutdown and we return the error.
+	err := <-shutdownChan
+	if err != nil {
+		return err
+	}
+	// Exiting....
+	app.logger.PrintInfo("stopped server", map[string]string{
+		"addr": srv.Addr,
+	})
 	return nil
 }
